45f43d72ae6530d5189524ba8970129e6ab816a6
Praveen Chauhan <sudarshanrajpit2002@gmail.com>
2022-11-05 18:26:19 +0530

    clocl

---
+++ clock.js
@@ -0,0 +1,12 @@
+showTime();
+function showTime()
+{
+let date = new Date();
+let h = date.getHours();
+let m = date.getMinutes();
+let s = date.getSeconds();
+let ms = date.getMilliseconds();
+
+let time = h+":"+m+":"+s+":"+ms;
+document.querySelector("#clock").innerHTML=time;setTimeout(showTime,1)
+}
